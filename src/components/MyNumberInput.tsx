82e7f6913576e6d187fe30315e6e00cee60aeb57
Marta Jurado <martaju29@gmail.com>
2023-10-26 11:04:54 +0200

    feat: add mantine to my library

---
+++ src/components/MyNumberInput.tsx
@@ -0,0 +1,27 @@
+import { MantineProvider, NumberInput, createTheme } from '@mantine/core';
+import { FC } from 'react';
+
+interface Props {
+    /**
+     * Text to display
+     */
+    label?: string
+}
+
+const theme = createTheme({
+    /** Put your mantine theme override here */
+    primaryColor: 'blue',
+    defaultRadius: 'md',
+});
+
+export const MyNumberInput : FC<Props> = () => {
+  return (
+    <MantineProvider theme={theme}>
+        <NumberInput
+        label="Input label"
+        description="Input description"
+        placeholder="Input placeholder"
+        />
+    </MantineProvider>
+  );
+}
--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import { MantineProvider, createTheme } from '@mantine/core';
+import '@mantine/core/styles.css';
+
+const theme = createTheme({
+  /** Put your mantine theme override here */
+  primaryColor: 'blue',
+  defaultRadius: 'md',
+});
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
