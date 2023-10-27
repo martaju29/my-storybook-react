76f21b2b78b6a09a60e0bebc44ca6642143fcbe7
Marta Jurado <martaju29@gmail.com>
2023-10-27 11:05:23 +0200

    fix: change config files

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
     lib: {
       entry: 'src/index.ts', // Asegúrate de que este sea tu punto de entrada principal
       name: 'mj-my-storybook-react-components', // Elige un nombre para tu biblioteca
-      fileName: (format) => `index.${format}.js` // nombre de archivo de salida
+      fileName: (format) => `mj-my-storybook-react-components.${format}.js` // nombre de archivo de salida
     },
     rollupOptions: {
       external: ['react', 'react-dom'],
